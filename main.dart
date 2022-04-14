842ff99c8730a8f747c2a4db2816ca6d056f5ac4
Mario <marioghbk@outlook.com>
2022-04-13 23:06:51 -0300

    Construtores e Construtores nomeados

--- main.dart
+++ main.dart
@@ -7,23 +7,46 @@ void main() {
   do {
     print("Menu: ");
     print("1 - Cadastrar Cliente");
-    print("2 - Lista Clientes");
+    print("2 - Cadastrar Cliente Masculino");
+    print("3 - Cadastrar Cliente Feminino");
+    print("4 - Lista Clientes");
     print("0 - Sair");
     opcao = int.parse(stdin.readLineSync()!);
     switch (opcao) {
       case 1:
-        var cliente = new Pessoa();
         print("Digite o nome do cliente: ");
-        cliente.nome = stdin.readLineSync();
+        var nome = stdin.readLineSync();
         print("Digite a idade do cliente: ");
-        cliente.idade = int.parse(stdin.readLineSync()!);
+        var idade = int.parse(stdin.readLineSync()!);
         print("Digite o email do cliente: ");
-        cliente.email = stdin.readLineSync();
+        var email = stdin.readLineSync();
         print("Digite o sexo do cliente: ");
-        cliente.sexo = stdin.readLineSync();
+        var sexo = stdin.readLineSync();
+
+        var cliente = new Pessoa(nome, idade, sexo, email);
         print(cliente.nome);
         break;
       case 2:
+        print("Digite o nome do cliente: ");
+        var nome = stdin.readLineSync();
+        print("Digite a idade do cliente: ");
+        var idade = int.parse(stdin.readLineSync()!);
+        print("Digite o email do cliente: ");
+        var email = stdin.readLineSync();
+
+        var cliente = new Pessoa.construtorMasculino(nome, idade, email);
+        print(cliente.sexo);
+        break;
+      case 3:
+        print("Digite o nome do cliente: ");
+        var nome = stdin.readLineSync();
+        print("Digite a idade do cliente: ");
+        var idade = int.parse(stdin.readLineSync()!);
+        print("Digite o email do cliente: ");
+        var email = stdin.readLineSync();
+
+        var cliente = new Pessoa.construtorFeminino(nome, idade, email);
+        print(cliente.sexo);
         break;
     }
   } while (opcao != 0);
--- pessoa.dart
+++ pessoa.dart
@@ -3,4 +3,23 @@ class Pessoa {
   int? idade;
   String? sexo;
   String? email;
+
+  Pessoa(String? nome, int? idade, String? sexo, String? email) {
+    this.nome = nome;
+    this.idade = idade;
+    this.sexo = sexo;
+    this.email = email;
+  }
+  Pessoa.construtorMasculino(String? nome, int? idade, String? email) {
+    this.nome = nome;
+    this.idade = idade;
+    this.email = email;
+    this.sexo = "Masculino";
+  }
+  Pessoa.construtorFeminino(String? nome, int? idade, String? email) {
+    this.nome = nome;
+    this.idade = idade;
+    this.email = email;
+    this.sexo = "Feminino";
+  }
 }
